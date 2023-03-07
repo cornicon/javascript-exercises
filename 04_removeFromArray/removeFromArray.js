8c7f459891e33d738505180833e8f4b3d753ac64
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-07 15:00:18 -0800

    Initial Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(InputArray, NumberToRemove) {
+    let result = [];
+    for (let i = 0; i < InputArray.length; i++)
+    {
+        if(InputArray[i] !== NumberToRemove)
+            result.push(InputArray[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
