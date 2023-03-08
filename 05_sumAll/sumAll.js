e2f082b6cd178a951e70b084ff65979e1b62dd69
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-07 17:24:10 -0800

    First Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(CountFromNumber, CountToNumber) {
+    let result = 0;
+    for (let i = CountFromNumber; i <= CountToNumber; i++)
+    {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
