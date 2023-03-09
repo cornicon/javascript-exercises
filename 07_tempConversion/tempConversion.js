7aee81d9a6279ae366ef65a8aa7cf07f68758018
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-09 13:53:35 -0800

    Fixed Rounding To Nearest Tenth

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function(TemperatureFahrenheit) {
   let result = (TemperatureFahrenheit - 32) * 5.0/9.0;
+  result = Math.round(result * 10) / 10;
   return result;
 };
 
 const convertToFahrenheit = function(TemperatureCelcius) {
   let result = (TemperatureCelcius * 9.0/5.0) + 32;
+  result = Math.round(result * 10) / 10;
   return result;
 };
 
