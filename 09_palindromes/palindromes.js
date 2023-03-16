727f25493ed6cd2837e1eb94c54aa58f75fa967e
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-16 14:41:36 -0700

    My Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    // remove all spaces and special characters
+    let sanitizedInput = inputString.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    const length = sanitizedInput.length;
+    for(let i = 0; i < length / 2; i++)
+    {
+        if(sanitizedInput.charAt(i) != sanitizedInput.charAt(length - 1 - i))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
