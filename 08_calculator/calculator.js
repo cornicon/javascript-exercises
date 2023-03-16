a1bc10651e76a240dbbec581673f1d813bbb92b0
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-16 13:43:49 -0700

    My Initial Solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+// return sum of an array
+const sum = function(arrayNumbers) {
+  let sum = 0;
+	for(let number in arrayNumbers)
+    sum += number;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNumbers) {
+  let result = 1;
+  for(let number in arrayNumbers)
+  {
+    result *= number; 
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = 1;
+  for (let i = 0; i < exponent; i++)
+  {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for(number; number > 1; i--)
+  {
+    result *= number;
+  }
+  return result;
 };
 
 // Do not edit below this line
