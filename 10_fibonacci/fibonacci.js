7bb2309dfd7849f0e968f8f8abe1d7f252ef6890
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-16 15:18:44 -0700

    Initial Solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let fibonacciNumberIndex = Number(input);
+    let fibArray = [1, 1];
+    for(let i = 0; i <= fibonacciNumberIndex - 2; i++)
+    {
+        fibArray.push(fibArray[i] + fibArray[i+1]);
+    }
+    return fibArray[fibonacciNumberIndex-1];
 };
 
 // Do not edit below this line
