73df87379735e7e8853b03bb08c6d54396ff874a
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-16 16:36:51 -0700

    Fixed If Person Still Living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,14 @@
 const findTheOldest = function(arrayPeople) {
     const oldest = arrayPeople.sort( function(a, b) {
-        const ageA = a.yearOfDeath - a.yearOfBirth;
-        const ageB = b.yearOfDeath - b.yearOfBirth;
+        let ageA, ageB;
+        if('yearOfDeath' in a)
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        else
+            ageA = new Date().getFullYear() - a.yearOfBirth;
+        if('yearOfDeath' in b)
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        else 
+            ageB = new Date().getFullYear() - b.yearOfBirth;
         
         return ageA > ageB ? -1 : 1;
     })
